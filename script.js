758a83828aa1ba641eb5c34e1c819c47d597ef78
EHFLAMESS <habbalema@gmail.com>
2025-02-02 23:04:50 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const section = document.querySelector("section");
+    section.addEventListener("click", function () {
+        alert("You clicked on the section!");
+    });
+});
